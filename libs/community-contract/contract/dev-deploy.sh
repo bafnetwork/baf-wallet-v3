8ff8b80604149e547538c0a3e5576e9e59cd4ca4
Lev Stambler <levstamb@gmail.com>
2021-06-10 16:25:51 -0400

    deploy script

--- libs/community-contract/contract/dev-deploy.sh
+++ libs/community-contract/contract/dev-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-near dev-deploy target/wasm32-unknown-unknown/release/community_contract.wasm | echo
-near call dev-1623353929808-71316364508823 new --accountId=levtester.testnet
-near call dev-1623353929808-71316364508823 add_admins --accountId=levtester.testnet --args '{"new_admins": ["levl333.testnet"]}'
+near dev-deploy target/wasm32-unknown-unknown/release/community_contract.wasm
+
+echo "{\"contractName\": \"$(cat neardev/dev-account)\"}" > ../config.json
+near call $(cat neardev/dev-account) new --accountId=levtester.testnet
+near call $(cat neardev/dev-account) add_admins --accountId=levtester.testnet --args '{"new_admins": ["levl333.testnet"]}'
