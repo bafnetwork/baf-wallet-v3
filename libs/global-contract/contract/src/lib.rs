b62ff85f6ab0f8e630af374cb74342ea129d866d
Lev Stambler <levstamb@gmail.com>
2021-06-24 14:32:07 -0400

    global-contr

--- libs/global-contract/contract/src/lib.rs
+++ libs/global-contract/contract/src/lib.rs
@@ -1,16 +1,20 @@
 pub mod account_info;
+pub mod admin;
+pub mod community_contract;
 pub mod errors;
 
 use crate::account_info::AccountInfos;
-use crate::errors::throw_error;
-use std::convert::TryInto;
 use crate::env::predecessor_account_id;
+use crate::errors::throw_error;
+use admin::Admin;
+use community_contract::CommunityContract;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedSet;
 use near_sdk::env::{current_account_id, is_valid_account_id, keccak256, signer_account_id};
 use near_sdk::AccountId;
 use near_sdk::PanicOnDefault;
 use near_sdk::{collections::UnorderedMap, env, near_bindgen};
+use std::convert::TryInto;
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc = near_sdk::wee_alloc::WeeAlloc::INIT;
@@ -30,6 +34,7 @@ pub struct GlobalData {
     pub(crate) account_infos: UnorderedMap<SecpPKInternal, AccountInfo>,
     pub(crate) admins: UnorderedSet<AccountId>,
     pub(crate) default_nft_contract: Option<AccountId>,
+    pub(crate) server_to_community_contract: UnorderedMap<String, AccountId>,
 }
 
 #[near_bindgen]
@@ -41,13 +46,35 @@ impl GlobalData {
         Self {
             admins: default_admins,
             account_infos: UnorderedMap::new("account-infos-map".as_bytes()),
+            server_to_community_contract: UnorderedMap::new(
+                "server-community-contract-map".as_bytes(),
+            ),
             default_nft_contract: None,
         }
     }
+
+    // TODO: and add tests
+    // pub fn add_admins()
+    // pub fn remove_admins()
+
     pub(crate) fn parse_secp_pk(secp_pk: SecpPK) -> Result<SecpPKInternal, String> {
         secp_pk
             .try_into()
             .map_err(|_e| "Incorrect public key format".to_string())
+    }
+}
+
+#[near_bindgen]
+impl CommunityContract for GlobalData {
+    fn set_community_contract(&mut self, server: String, contract_address: AccountId) {
+        if !(self.admins.contains(&predecessor_account_id())) {
+            throw_error(crate::errors::UNAUTHORIZED);
+        }
+        self.server_to_community_contract
+            .insert(&server, &contract_address);
+    }
+    fn get_community_contract(&self, server: String) -> Option<AccountId> {
+        self.server_to_community_contract.get(&server)
     }
 }
 
@@ -75,12 +102,14 @@ impl AccountInfos for GlobalData {
             throw_error(crate::errors::INVALID_ACCOUNT_ID);
         }
 
-        if !(predecessor_account_id() == new_account_id
-            || self.admins.contains(&predecessor_account_id()))
-        {
-            throw_error(crate::errors::UNAUTHORIZED);
-        }
+        // TODO: lets think about this. We could j remove the need for admins
+        // if !(predecessor_account_id() == new_account_id
+        //     || self.admins.contains(&predecessor_account_id()))
+        // {
+        //     throw_error(crate::errors::UNAUTHORIZED);
+        // }
 
+        // TODO: how would this work if someone else is calling this for someone else?
         let (secp_pk_internal, nonce) = self.verify_sig(user_id, secp_pk, secp_sig_s);
         self.account_infos.insert(
             &secp_pk_internal,
@@ -98,4 +127,27 @@ impl AccountInfos for GlobalData {
         self.account_infos.remove(&secp_pk_internal);
     }
 }
+#[near_bindgen]
+impl Admin for GlobalData {
+    fn add_admins(&mut self, new_admins: Vec<AccountId>) {
+        if !self.admins.contains(&predecessor_account_id()) {
+            throw_error(crate::errors::UNAUTHORIZED);
+        }
+        for admin in new_admins {
+            self.admins.insert(&admin);
+        }
+    }
 
+    fn remove_admins(&mut self, admins: Vec<AccountId>) {
+        if !self.admins.contains(&predecessor_account_id()) {
+            throw_error(crate::errors::UNAUTHORIZED);
+        }
+        for admin in admins {
+            self.admins.remove(&admin);
+        }
+    }
+
+    fn get_admins(&self) -> Vec<AccountId> {
+        self.admins.to_vec()
+    }
+}
