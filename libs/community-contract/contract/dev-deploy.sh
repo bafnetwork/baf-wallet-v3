e57b1c38c92223fa7db322696815da27e8776814
Lev Stambler <levstamb@gmail.com>
2021-07-16 14:15:14 -0400

    remove comm contr

--- libs/community-contract/contract/dev-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-near dev-deploy target/wasm32-unknown-unknown/release/community_contract.wasm
-
-echo "{\"contractName\": \"$(cat neardev/dev-account)\"}" > ../config.json
-near call $(cat neardev/dev-account) new --accountId=sladuca2.testnet
-near call $(cat neardev/dev-account) add_admins --accountId=sladuca2.testnet --args '{"new_admins": ["levl333.testnet", "sladuca2.testnet"]}'
