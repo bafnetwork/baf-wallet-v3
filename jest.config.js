5c42cdf5b5b14996adbc24947a6b7243264fec59
Sebastien La Duca <sladuca777@gmail.com>
2022-03-21 22:33:13 -0500

    detangle dependency ratsnest

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/libs/interfaces',
-    '<rootDir>/apps/api',
-    '<rootDir>/libs/api-client',
-    '<rootDir>/apps/bot',
-    '<rootDir>/libs/redirect-generator',
-    '<rootDir>/libs/chain-info',
-    '<rootDir>/apps/frontend',
-    '<rootDir>/libs/near',
-    '<rootDir>/libs/utils',
-    '<rootDir>/libs/torus',
-    '<rootDir>/libs/crypto',
-    '<rootDir>/libs/base-components',
-    '<rootDir>/libs/errors',
-    '<rootDir>/libs/global-state',
-    '<rootDir>/libs/global-contract',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
